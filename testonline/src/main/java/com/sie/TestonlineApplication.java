1bb0fc8448112c90c107a92fd486772bf50ee917
testonline <806125872@qq.com>
2018-08-28 13:50:54 +0800

    项目模板

---
+++ testonline/src/main/java/com/sie/TestonlineApplication.java
@@ -0,0 +1,12 @@
+package com.sie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestonlineApplication.class, args);
+	}
+}

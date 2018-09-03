cb8bce980c24452f549eb018316008bf7cd74175
testonline <806125872@qq.com>
2018-09-03 16:59:38 +0800

    增加admin模块 教师管理 dao层功能

--- testonline/src/main/java/com/sie/mapper/AdminMapper/TeacherManageMapper.java
+++ testonline/src/main/java/com/sie/mapper/AdminMapper/TeacherManageMapper.java
@@ -1,5 +1,19 @@
 package com.sie.mapper.AdminMapper;
 
-public interface TeacherManageMapper {
+import com.sie.domain.Teacher;
 
+import java.util.List;
+
+public interface TeacherManageMapper {
+    public void addTeacher(Teacher teacher);
+
+    public void deleteTeacher(String num);
+
+    public void updateTeacher(Teacher teacher);
+
+    public Teacher findTeacherByNum(String num);
+
+    public List<Teacher> findAllTeacher();
+
+    public int findCount();
 }

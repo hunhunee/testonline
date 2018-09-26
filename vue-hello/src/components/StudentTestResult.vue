b9674d92ffeaacdd69230eb7272c21c5a71bc2a5
testonline <806125872@qq.com>
2018-09-26 20:58:10 +0800

    修改 merge 冲突

--- vue-hello/src/components/StudentTestResult.vue
+++ vue-hello/src/components/StudentTestResult.vue
@@ -4,13 +4,7 @@
 <template>
     <div class="layout">
      学生查询成绩
-<<<<<<< HEAD
- 
-=======
-=======
-
->>>>>>> 4de392af628d8bf53ffa48fdfa83fb0a0ab1c98c
->>>>>>> cgwBranch
+	 
     </div>
 </template>
 <script>

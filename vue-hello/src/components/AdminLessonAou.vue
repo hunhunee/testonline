c1f53c043096db9ac27198c0085c3f79e21d73ec
hunhunee <806125872@qq.com>
2018-10-23 15:03:03 +0800

    修改

--- vue-hello/src/components/AdminLessonAou.vue
+++ vue-hello/src/components/AdminLessonAou.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="userLogin">
     <div class="text" >
-      科目设置----添加科目
+      科目设置----添加、修改科目信息
     </div>
     <Form ref="formInline" :model="formInline" :rules="ruleInline" :label-width="400">
 
@@ -55,25 +55,37 @@ export default {
                 },
                 ruleInline: {
                     lesId: [
-                        { required: true, message: 'Please fill in the lesson Id', trigger: 'blur' },
-                        { type: 'number', min: 1, message: 'classId必须为数字', trigger: 'blur' }
+                       { required: true, message: 'Please fill in the lesson Id.', trigger: 'blur' },
+                        { validator: this.validateMobile,trigger: 'blur' }
                     ],
                     lesName: [
                         { required: true, message: 'Please fill in the lesson Name.', trigger: 'blur' },
                     ],
 
                 },
-              modal:false
+              modal:false,
+
             }
         },
         methods: {
+          validateMobile :(rule, value, callback) => {
+
+           if (!Number.isInteger(+value)) {
+               callback(new Error('请输入数字值'));
+           } else {
+               callback();
+           }
+       },
 
               handleSubmit(name) {
 
                 this.$refs[name].validate((valid) => {
-
+                    console.log(this.formInline.lesId);
+                    console.log();
                     if (valid) {
+
                       if(this.$store.state.lesson.utaction=="2"){
+
                         let data = {
                             "lesId": this.formInline.lesId,
                             "lesName":this.formInline.lesName,
@@ -100,11 +112,7 @@ export default {
                           }
 
                         },1200)
-
-
-
                       }
-
                     } else {
                         this.$Message.error('表单数据不能为空!');
                     }
@@ -121,11 +129,8 @@ export default {
       },
           ok:function(){
             this.modal = false
-
-
           },
           cancel:function(){
-
           }
 
     }
